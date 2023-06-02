c4e2de8fcc39effc13b342a3f60d424573c5da43
dinesh subhash pawar <dspfamily8@gmail.com>
2023-06-02 15:48:57 +0530

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    mongoose.connect("mongodb://0.0.0.0:27017/crud-app");
+    // mongoose.connect("mongodb://0.0.0.0:27017/crud-app");
+    mongoose.connect(process.env.MONGO_URL);
+
     console.log("db is connected now");
   } catch (error) {
     console.log("not connected");
